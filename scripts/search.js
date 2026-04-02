0fe92468b1e2a7663189c811e4e9aba886e830ab
AshaJenvasu <chaiyawatjenvasu@gmail.com>
2026-04-02 15:54:44 +0700

    add: about us page with Tailwind css

--- scripts/search.js
+++ scripts/search.js
@@ -3,13 +3,21 @@ const searchBtn = document.querySelector("#search-btn");
 const searchPopup = document.querySelector("#search-popup");
 const closeSearch = document.querySelector("#close-search");
 
-searchBtn.addEventListener("click", () => {
-  searchPopup.classList.add("active");
-});
+if (searchBtn && searchPopup && closeSearch) {
+  searchBtn.addEventListener("click", () => {
+    searchPopup.classList.add("active");
+  });
 
-closeSearch.addEventListener("click", () => {
-  searchPopup.classList.remove("active");
-});
+  closeSearch.addEventListener("click", () => {
+    searchPopup.classList.remove("active");
+  });
+
+  searchPopup.addEventListener("click", (event) => {
+    if (event.target === searchPopup) {
+      searchPopup.classList.remove("active");
+    }
+  });
+}
 
 // ==== SET UP ELEMENTS =====
 const searchInput = document.querySelector("#search-popup input");
